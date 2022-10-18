203a26f1d85415746942fa7c8dc4f6003d058029
Sellbat <wbatsell05@gmail.com>
2022-10-18 16:01:28 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,55 @@ public class Player {
     private double money;
     private Link position;
     private Boolean jailed;
+    private int turnsInJail;
     private ArrayList<BoardSpace> properties;
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Player.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public Link getPosition() {
+        return position;
+    }
+
+    public void setPosition(Link position) {
+        this.position = position;
+    }
+
+    public Boolean getJailed() {
+        return jailed;
+    }
+
+    public void setJailed(Boolean jailed) {
+        this.jailed = jailed;
+    }
+
+    public int getTurnsInJail() {
+        return turnsInJail;
+    }
+
+    public void setTurnsInJail(int turnsInJail) {
+        this.turnsInJail = turnsInJail;
+    }
+
+    public ArrayList<BoardSpace> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ArrayList<BoardSpace> properties) {
+        this.properties = properties;
+    }
 
     public Player(String playerName){
         money = 1500.00;
@@ -15,8 +63,7 @@ public class Player {
     public boolean land(BoardSpace spot){
         if (spot.purchasable) {
             System.out.println("Would you like to purchase this land?");
-
-
+            
         }
 
     }
